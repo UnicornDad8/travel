6156327a9afff412533cc685866ad74ebc213674
UnicornDad8 <unicorndad8@gmail.com>
2023-07-12 16:41:41 -0400

    Installed dependencies that supports vanilla css and configure metro.config.js and babel.config.js

--- App.jsx
+++ App.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {Text, View} from 'react-native';
+import styles from './App.module.css';
 
 function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello world!</Text>
+      <Text style={styles.text}>Hello world!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 export default App;
