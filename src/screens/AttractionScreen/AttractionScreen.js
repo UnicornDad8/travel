bf360d21be015a9fc5f9b84e916a1365575d756c
UnicornDad8 <unicorndad8@gmail.com>
2023-09-19 09:07:08 -0400

    Adding ImageBackground to AttractionScreen

--- src/screens/AttractionScreen/AttractionScreen.js
+++ src/screens/AttractionScreen/AttractionScreen.js
@@ -1,9 +1,20 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
-import './AttractionScreen.module.css';
+import {
+  SafeAreaView,
+  View,
+  ImageBackground,
+  Text,
+  Dimensions,
+} from 'react-native';
+import styles from './AttractionScreen.module.css';
+
+const {height} = Dimensions.get('window');
 
 const AttractionScreen = ({navigation, route}) => {
   const {item} = route?.params || {};
+  const mainImage = item?.images.length ? item?.images[0] : null;
+
+  console.log('mainImage', mainImage);
 
   const onBack = () => {
     navigation.goBack();
@@ -11,10 +22,17 @@ const AttractionScreen = ({navigation, route}) => {
 
   return (
     <SafeAreaView>
-      <Text onPress={onBack} style={{margin: 32}}>
-        BACK
-      </Text>
-      <Text>{item?.name}</Text>
+      <View style={styles.container}>
+        <ImageBackground
+          style={{width: '100%', height: height / 2}}
+          imageStyle={{borderRadius: 20}}
+          source={{uri: mainImage}}
+        />
+        <Text onPress={onBack} style={{margin: 32}}>
+          BACK
+        </Text>
+        <Text>{item?.name}</Text>
+      </View>
     </SafeAreaView>
   );
 };
