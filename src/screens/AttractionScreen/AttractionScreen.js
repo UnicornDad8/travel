c75ed36f35d1353db240972a021d4c62c80bb7ec
UnicornDad8 <unicorndad8@gmail.com>
2023-09-19 08:33:08 -0400

    Handling back event in AttractionScreen

--- src/screens/AttractionScreen/AttractionScreen.js
+++ src/screens/AttractionScreen/AttractionScreen.js
@@ -2,10 +2,13 @@ import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
 import './AttractionScreen.module.css';
 
-const AttractionScreen = ({route}) => {
+const AttractionScreen = ({navigation, route}) => {
   const {item} = route?.params || {};
   return (
     <SafeAreaView>
+      <Text onPress={() => navigation.goBack()} style={{margin: 32}}>
+        BACK
+      </Text>
       <Text>{item?.name}</Text>
     </SafeAreaView>
   );
