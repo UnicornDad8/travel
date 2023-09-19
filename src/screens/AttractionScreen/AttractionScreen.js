28c4c175bdbc3accfcfb80673292551d08102610
UnicornDad8 <unicorndad8@gmail.com>
2023-09-19 08:41:09 -0400

    Back event on AttractionCard better performance

--- src/screens/AttractionScreen/AttractionScreen.js
+++ src/screens/AttractionScreen/AttractionScreen.js
@@ -4,9 +4,14 @@ import './AttractionScreen.module.css';
 
 const AttractionScreen = ({navigation, route}) => {
   const {item} = route?.params || {};
+
+  const onBack = () => {
+    navigation.goBack();
+  };
+
   return (
     <SafeAreaView>
-      <Text onPress={() => navigation.goBack()} style={{margin: 32}}>
+      <Text onPress={onBack} style={{margin: 32}}>
         BACK
       </Text>
       <Text>{item?.name}</Text>
