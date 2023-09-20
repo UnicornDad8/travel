efd94329a4b99470cd383b740e513e564157a96e
UnicornDad8 <unicorndad8@gmail.com>
2023-09-20 09:59:00 -0400

    Added GalleryScreen with all the images

--- src/screens/GalleryScreen/GalleryScreen.js
+++ src/screens/GalleryScreen/GalleryScreen.js
@@ -1,11 +1,39 @@
 import React from 'react';
-import {SafeAreaView, View, Text} from 'react-native';
-import './GalleryScreen.module.css';
+import {
+  SafeAreaView,
+  View,
+  FlatList,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import styles from './GalleryScreen.module.css';
 
-const GalleryScreen = () => {
+const GalleryScreen = ({navigation, route}) => {
+  const {images} = route?.params || {};
+
+  const onBack = () => {
+    navigation.goBack();
+  };
+
   return (
-    <SafeAreaView>
-      <Text>Gallery Screen</Text>
+    <SafeAreaView style={styles.container} scrollI>
+      <View style={styles.gallery}>
+        <FlatList
+          data={images}
+          style={styles.imagesContainer}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={item => item}
+          renderItem={({item}) => (
+            <Image key={item} source={{uri: item}} style={styles.image} />
+          )}
+        />
+        <TouchableOpacity onPress={onBack} style={styles.backContainer}>
+          <Image
+            source={require('../../assets/back.png')}
+            style={styles.backIcon}
+          />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 };
